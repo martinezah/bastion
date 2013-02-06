1d89cf237556adfbf1f3d5047cca852b1f936f1e
Marti Martinez <marti@argenticsoftware.com>
2013-02-06 14:31:30 -0800

    updating config syntax and retention process

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ bastion
 
 EC2 Rotating Backup Framework
 
-Bastion is a simple framework for automating backup rotations of EC2 volumes. It allows your to specify multiple backup rotation schemes, and apply those to the servers in your infrastructure. A cron job manages the creation of snapshots as well as rotation of older backups to offline storage (AWS Glacier) and purging of old backups.
+Bastion is a simple framework for automating backup rotations of EC2 volumes. It allows your to specify multiple backup rotation schemes, and apply those to the servers in your infrastructure. A cron job manages the creation of new snapshots as well as retention and purging of older snapshots.
 
 Configuration
 -------
 
-Configuration is managed through a simple json file, with three sections. A sample configuration file, sample-config.json, is included. The first section, "credentials", holds the AWS credentials used to access the EC2 and Glacier API. The second, "schemes", describes the available backup rotation schemes, and the third, "instances", maps EC2 instances to the appropriate schemes. Instances can be referred to by their instance ID, or by their "Name" tag if one has been assigned in AWS.
+Configuration is managed through a simple json file, with three sections. A sample configuration file, sample-config.json, is included. The first section, "credentials", holds the AWS credentials used to access the EC2 API. The second, "schemes", describes the available backup/retention schemes, and the third, "instances", maps EC2 instances to the appropriate schemes. Instances can be referred to by their instance ID, or by their "Name" tag if one has been assigned in AWS.
 
 Deployment
 -------
--- bastion.py
+++ bastion.py
@@ -7,18 +7,35 @@ from datetime import datetime
 from boto.ec2.connection import EC2Connection
 
 def backup(vol, sys, scheme):
-    if (stamp.hour % scheme["snapshots"]["period"] == 0):
+    if (stamp.hour % scheme["period"] == 0):
         nickname = '{}_{}_{}'.format(sys, vol.id, stamp.isoformat())
         print "Creating Snapshot:", nickname
         ec2.create_snapshot(vol.id, nickname)
         snap_list = vol.snapshots()
         snap_list.sort(lambda x, y: int(math.ceil((datetime.strptime(x.start_time, "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.strptime(y.start_time, "%Y-%m-%dT%H:%M:%S.%fZ")).total_seconds())), reverse = True)
-        if ("archive" in scheme.keys()):
-            pass
-        if (len(snap_list) > scheme["snapshots"]["retain"]):
-            for ii in range(scheme["snapshots"]["retain"], len(snap_list)):
-                print "Deleting Snapshot:", snap_list[ii].description
-                snap_list[ii].delete()
+        weekly = scheme["retention"]["weekly"]
+        daily  = scheme["retention"]["daily"]
+        hourly = scheme["retention"]["hourly"]
+        for snap in snap_list:
+            when = datetime.strptime(snap.start_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+            if (when.weekday() == 0):
+                if (weekly > 0):
+                    weekly -= 1
+                else:
+                    print "Deleting Snapshot:", snap.description
+                    snap.delete()
+            elif (when.hour == 0):
+                if (daily > 0):
+                    daily -= 1
+                else:
+                    print "Deleting Snapshot:", snap.description
+                    snap.delete()
+            else:
+                if (hourly):
+                    hourly -= 1
+                else:
+                    print "Deleting Snapshot:", snap.description
+                    snap.delete()
 
 stamp = datetime.now()
 try: 
